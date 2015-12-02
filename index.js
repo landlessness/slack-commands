e4e67368d2280a298b6396355b2f2d2a92be469f
landlessness <brian@landlessness.net>
2015-12-01 23:37:46 -0500

    less code

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 var argo = require('argo');
 var querystring = require('querystring');
-
 var lcd = require('request');
 
 var lcdOptions = { 
   method: 'POST',
   url: 'http://zetta-cloud-2.herokuapp.com/servers/Detroit/devices/0d919602-8755-4727-9832-970efc17c63d',
-  headers: 
-  { 'content-type': 'application/x-www-form-urlencoded',
-  'cache-control': 'no-cache' },
-  form: { action: 'change' } };
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  form: { action: 'change' }
+};
 
 argo()
-.get('^/test$', function(handle) {
-  handle('request', function(env, next) {
-    env.response.statusCode = 200;
-    env.response.body = { status: ['OK'] };
-    next(env);
-  });  
-})
 .post('^/display$', function(handle) {
   handle('request', function(env, next) {
     var fullBody = '';
@@ -27,16 +18,10 @@ argo()
     });
     env.request.on('end', function() {
       var decodedBody = querystring.parse(fullBody);
-      console.log(fullBody);
-      console.log(decodedBody);
-      var fullMessage = '"' + decodedBody.text + '" -@' + decodedBody.user_name;
-      console.log('changing display to: ' + fullMessage);
-
-      lcdOptions.form.message = fullMessage;
+      lcdOptions.form.message = '"' + decodedBody.text + '" -@' + decodedBody.user_name;
 
       lcd(lcdOptions, function (error, response, body) {
         if (error) throw new Error(error);
-        console.log(body);
       });
 
       env.response.statusCode = 200;
