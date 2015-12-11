8b4ef025158e870b1e5f93c4b861db3fb7155678
landlessness <brian@landlessness.net>
2015-12-11 14:53:59 -0500

    better logging

--- index.js
+++ index.js
@@ -21,37 +21,46 @@ argo()
       }
       env.request.body = querystring.parse(body.toString());
       var displayMessage = '"' + env.request.body.text + '" -@' + env.request.body.user_name;
+      console.log('querying for the LCD display...');
       zetta.observe(displayQuery, function(display) {
+        console.log('got LCD display. sending message to LCD display...: ' +  displayMessage);
         display.call('change', displayMessage, function(err) {
           if (err) {
+            console.log('failed to send message');
             console.error(err);
+            // API RESPONSE
+            env.response.statusCode = 400;
+            next(env);
           } else {
-            console.log('success');
+            console.log('successfully sent message');
+            // Slack WebHook Response
+            var webhookResponse = {
+              text: '@' + env.request.body.user_name + ' posted to /display.',
+              attachments: [
+                {
+                  text: 'view: https://video.nest.com/live/zettajs'
+                }
+              ]
+            };
+            console.log('sending inbound webhook to slack...');
+            request.post(
+              env.request.body.response_url,
+              webhookResponse,
+              function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  console.log('successfully sent webhook to slack');
+                  console.log(body)
+                } else {
+                  console.log('failed to send webhook to slack');
+                }
+              }
+            );
+            // API RESPONSE
+            env.response.statusCode = 200;
+            next(env);
           }
         });
       });
-      // Into Slack WebHook Response
-      var webhookResponse = {
-        text: '@' + env.request.body.user_name + ' posted to /display.',
-        attachments: [
-          {
-            text: 'view: https://video.nest.com/live/zettajs'
-          }
-        ]
-      };
-      console.log('sending inbound webhook...');
-      request.post(
-        env.request.body.response_url,
-        webhookResponse,
-        function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            console.log(body)
-          }
-        }
-      );
-      // API RESPONSE
-      env.response.statusCode = 200;
-      next(env);
     });
   });
 })
